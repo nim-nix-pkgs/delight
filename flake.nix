b69b12c8d7c7cd73f80f92ebfddfe8ab7c2c2bb3
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:58 -0300

    chore: re index delight

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."delight-master".dir   = "master";
+  inputs."delight-master".owner = "nim-nix-pkgs";
+  inputs."delight-master".ref   = "master";
+  inputs."delight-master".repo  = "delight";
+  inputs."delight-master".type  = "github";
+  inputs."delight-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."delight-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."delight-0_1_0".dir   = "0_1_0";
+  inputs."delight-0_1_0".owner = "nim-nix-pkgs";
+  inputs."delight-0_1_0".ref   = "master";
+  inputs."delight-0_1_0".repo  = "delight";
+  inputs."delight-0_1_0".type  = "github";
+  inputs."delight-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."delight-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
